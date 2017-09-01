83eb6f2a08259b35e6111259e11be4a22edfebcc
SAKryukov <github@sakryukov.org>
2017-09-01 19:11:05 -0400

    Added reference links in comments

--- docs/keyboard/ui.js
+++ docs/keyboard/ui.js
@@ -1,4 +1,14 @@
-"use strict";
+// Microtonal Music Study with Chromatic Lattice Keyboard
+//
+// Copyright (c) Sergey A Kryukov, 2017
+//
+// http://www.SAKryukov.org
+// http://www.codeproject.com/Members/SAKryukov
+// https://github.com/SAKryukov
+// https://github.com/SAKryukov/microtonal-chromatic-lattice-keyboard
+//
+// Original publication:
+// https://www.codeproject.com/Articles/1204180/Microtonal-Music-Study-Chromatic-Lattice-Keyboard"use strict";
 
 const keyboardHandler = (function () {
 
--- docs/webaudiofont/player.js
+++ docs/webaudiofont/player.js
@@ -1,4 +1,20 @@
-"use strict";
+// Microtonal Music Study with Chromatic Lattice Keyboard
+//
+// Copyright (c) Sergey A Kryukov, 2017
+//
+// http://www.SAKryukov.org
+// http://www.codeproject.com/Members/SAKryukov
+// https://github.com/SAKryukov
+// https://github.com/SAKryukov/microtonal-chromatic-lattice-keyboard
+//
+// Original publication:
+// https://www.codeproject.com/Articles/1204180/Microtonal-Music-Study-Chromatic-Lattice-Keyboard"use strict";
+
+//
+// This unit is based on:
+// https://github.com/surikov/webaudiofont
+// https://github.com/surikov/webaudiofontdata
+// 
 
 function WebAudioFontPlayer() {
 
