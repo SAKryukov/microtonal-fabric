885353df643b8c2293eb617f536a6adef3a2adf0
SAKryukov <github@sakryukov.org>
2017-09-29 11:35:53 -0400

    Added return to handleGoodBrowser in case of bad browser

--- docs/ui/good-browser.js
+++ docs/ui/good-browser.js
@@ -7,9 +7,9 @@ function handleGoodBrowser(scripts) {
 
     if (navigator.appName.indexOf("Microsoft") >= 0) { // bad browser
         while (document.body.lastChild) document.body.removeChild(document.body.lastChild);
-        //document.body.style.fontSize = "200%";
         document.body.style.padding = "1em";
         document.write("<p>Oh, " + navigator.appName + "? Nice try&hellip; <big>&#x263a;</big></p><br/><p>Sorry, such thing cannot be supported, as it does not provide minimal functionality required. Please use one of the major Web browsers.</p><br/>");
+        return;
     } //if
 
     var currentOrder = 0;   
