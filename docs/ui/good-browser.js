324ef6cfb01d5af8adab2a190ea44b0f30fe6d92
SAKryukov <github@sakryukov.org>
2017-09-29 04:18:56 -0400

    New technique: for bad browsers, good-browser.js is used: other scripts are loaded 1) dynamically, 2) in order

---
+++ docs/ui/good-browser.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+function handleGoodBrowser(scripts) {
+
+    // no "const", "let", lambda-like syntax () => {}, not "for... of Object",
+    // no String.prototype.includes -- it won't work with some bad browsers    
+
+    if (navigator.appName.indexOf("Microsoft") >= 0) { // bad browser
+        while (document.body.lastChild) document.body.removeChild(document.body.lastChild);
+        document.write("<br/><p>Oh, " + navigator.appName + "? Nice try&hellip; <big>&#x263a;</big></p><br/><p>Sorry, such thing cannot be supported, as it does not provide minimal functionality required. Please use one of the major Web browsers.</p>");
+    } //if
+
+    var currentOrder = 0;   
+    function loadScript(){
+        if (currentOrder == scripts.length)
+            return;
+        var scriptElement = document.createElement('script');
+        scriptElement.src = scripts[currentOrder];
+        scriptElement.onload = function() {
+            currentOrder++;
+            loadScript();
+        }; //scriptElement.onload
+        document.body.appendChild(scriptElement);
+    }; //loadScript
+    loadScript();       
+
+} //handleBadBrowser
