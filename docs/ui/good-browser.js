1aebebaee0cd364df1fbee72561a287c8c94b6e5
SAKryukov <github@sakryukov.org>
2017-09-30 03:55:52 -0400

    Improved detection and notification of incompatible browsers in good-browser.js: now it's not Microsoft, it's just the browser failed to load all scripts

--- docs/ui/good-browser.js
+++ docs/ui/good-browser.js
@@ -1,29 +1,50 @@
 ﻿"use strict";
 
-function handleGoodBrowser(scripts) {
+function handleGoodBrowser(scripts, successAction, errorAction) {
 
     // no "const", "let", lambda-like syntax () => {}, not "for... of Object",
     // no String.prototype.includes -- it won't work with some bad browsers    
 
-    if (navigator.appName.indexOf("Microsoft") >= 0) { // bad browser
-        while (document.body.lastChild) document.body.removeChild(document.body.lastChild);
-        document.body.style.padding = "1em";
-        document.write("<p>Oh, " + navigator.appName + "? Nice try&hellip; <big>&#x263a;</big></p><br/><p>Sorry, such thing cannot be supported, as it does not provide minimal functionality required. Please use one of the major Web browsers.</p><br/>");
-        return;
-    } //if
-
-    var currentOrder = 0;   
-    function loadScript(){
+    var saveWindowErrorHandler = window.onerror;
+    var saveBodyLoadHandler = document.body.onload;
+    var hasError = false;
+    window.onerror = function (event) {
+        hasError = true;
+    };
+    var currentOrder = 0;
+    function loadScript() {
         if (currentOrder == scripts.length)
             return;
-        var scriptElement = document.createElement('script');
+        var scriptElement = document.createElement("script");
         scriptElement.src = scripts[currentOrder];
-        scriptElement.onload = function() {
+        scriptElement.onload = function () {
             currentOrder++;
             loadScript();
         }; //scriptElement.onload
+        scriptElement.onerror = function () {
+            throw new URIError("error");
+        }; //scriptElement.onerror
         document.body.appendChild(scriptElement);
     }; //loadScript
-    loadScript();       
+    loadScript();
+    window.onerror = window.onerror;
+    document.body.onload = function () {
+        if (hasError && errorAction)
+            errorAction();
+        else if (!hasError && successAction)
+            successAction();
+        document.body.onload = saveBodyLoadHandler;
+    }; //if hasError
 
 } //handleGoodBrowser
+
+function incompatibleMessage(message, style) {
+    while (document.body.lastChild) document.body.removeChild(document.body.lastChild);
+    document.body.style.padding = "1em";
+    var text = document.createElement("p");
+    text.innerHTML = message;
+    if (style)
+        for (styleIndex in style)
+            text.style[styleIndex] = style[styleIndex];
+    document.body.appendChild(text);
+} //incompatibleMessage
