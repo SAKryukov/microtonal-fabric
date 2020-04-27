c8819d0e1c16823f32181a1f4a5abc6b222a05a3
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-04-27 01:36:16 -0400

    Ogolevets-Brainin keyboard: UI: key hightlights

--- docs/Ogolevets-Brainin-keyboard/ui/keyboard.js
+++ docs/Ogolevets-Brainin-keyboard/ui/keyboard.js
@@ -6,6 +6,11 @@ class Keyboard {
 
     constructor(controls) {
         const keyMap = new Map();
+        const handler = (element, on) => {
+            if (this.#implementation.keyHandler)
+            this.#implementation.keyHandler(keyMap.get(element), on);
+            element.style.fill = on ? "Chartreuse" : "white";
+        }
         const keys = controls.keyArray;
         let parseKeyNumberSlice = undefined;
         const parseKeyNumber = id => {
@@ -22,10 +27,10 @@ class Keyboard {
         for (let key of keys) {
             const index = parseKeyNumber(key.id);
             keyMap.set(key, index);
-            key.onmousedown = event => this.#implementation.keyHandler(keyMap.get(event.target), true);
-            key.onmouseup = event => this.#implementation.keyHandler(keyMap.get(event.target), false);
-            key.onmouseenter = event => { if (event.buttons == 1) this.#implementation.keyHandler(keyMap.get(event.target), true); }
-            key.onmouseleave = event => { if (event.buttons == 1) this.#implementation.keyHandler(keyMap.get(event.target), false); }             
+            key.onmousedown = event => handler(event.target, true);
+            key.onmouseup = event => handler(event.target, false);
+            key.onmouseenter = event => { if (event.buttons == 1) handler(event.target, true); }
+            key.onmouseleave = event => { if (event.buttons == 1) handler(event.target, false); }             
         } //loop
         this.#implementation.getFirst = _ => { return 0; }
         this.#implementation.getLast = _ => { return keys.length - 1; }
