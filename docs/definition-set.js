ff66bc5ea4ac36a2ba3059ed4cb7724b6d4d2a40
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-05-20 11:42:59 -0400

    Unit tests: relocated

--- docs/definition-set.js
+++ docs/definition-set.js
@@ -6,9 +6,6 @@
 // http://www.codeproject.com/Members/SAKryukov
 // https://github.com/SAKryukov
 // https://github.com/SAKryukov/microtonal-chromatic-lattice-keyboard
-//
-// Original publication:
-// https://www.codeproject.com/Articles/1204180/Microtonal-Music-Study-Chromatic-Lattice-Keyboard
 
 "use strict";
 
