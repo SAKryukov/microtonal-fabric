fc74c6f9d84120f853dcbee552d3b49344d030d6
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-12-04 19:03:52 -0500

    ui.components: SVG complete, texted on Kite.Giedraitis prototype

--- docs/Kite.Giedraitis/ui/keyboard.js
+++ docs/Kite.Giedraitis/ui/keyboard.js
@@ -5,12 +5,21 @@ class Keyboard {
     constructor (parent) {
         const svg = new SVG({
             x: { from: -1000, to: +1000, },
-            y: { from: -1000, to: +1000, } });
-        svg.line(-100, 100, 0, 200);
+            y: { from: -400, to: +400, } });
+        svg.element.style.width = "100%";
+        svg.element.style.height = "auto";
+        const line1 = svg.line(-100, 100, 0, 200);
         parent.appendChild(svg.element);
         svg.lineStrokeColor = "red";
         svg.line(-100, 100, 10, 210);
+        svg.text(200, 100, "my text", "820%");
+        svg.circleStrokeColor = "red";
+        svg.circleStrokeWidth = "1%";
+        svg.circle(0, 0, 300);
+        svg.rectangleStrokeWidth = "1%";
+        svg.rectangleFillColor = "yellow";
+        svg.rectangleStrokeColor = "Navy";
+        svg.rectangle(-500, 0, 300, 200, 0, 0);
     } //constructor
-
 
 } //class Keyboard
--- docs/ui.components/SVG.js
+++ docs/ui.components/SVG.js
@@ -13,9 +13,12 @@ class SVG {
         // defaults:
         this.#implementation.lineStrokeColor = "black";
         this.#implementation.circleStrokeColor = "black";
+        this.#implementation.rectangleStrokeColor = "black";
         this.#implementation.circleFillColor = "transparent";
+        this.#implementation.rectangleFillColor = "transparent";
         this.#implementation.lineStrokeWidth = null;
         this.#implementation.circleStrokeWidth = null;
+        this.#implementation.rectangleStrokeWidth = null;
         this.#implementation.svg = this.#implementation.createNS("svg");
         this.#implementation.svg.setAttribute("viewBox",
             `${viewBox.x.from} ${viewBox.y.from} ${viewBox.x.to - viewBox.x.from} ${viewBox.y.to - viewBox.y.from}`);
@@ -25,16 +28,61 @@ class SVG {
 
     get lineStrokeColor() { return this.#implementation.lineStrokeColor; }
     set lineStrokeColor(value) { this.#implementation.lineStrokeColor = value; }
+    get rectangleStrokeColor() { return this.#implementation.rectangleStrokeColor; }
+    set rectangleStrokeColor(value) { this.#implementation.rectangleStrokeColor = value; }
     get circleStrokeColor() { return this.#implementation.circleStrokeColor; }
     set circleStrokeColor(value) { this.#implementation.circleStrokeColor = value; }
+    get circleFillColor() { return this.#implementation.circleFillColor; }
+    set circleFillColor(value) { this.#implementation.circleFillColor = value; }
+    get rectangleFillColor() { return this.#implementation.rectangleFillColor; }
+    set rectangleFillColor(value) { this.#implementation.rectangleFillColor = value; }
+    get lineStrokeWidth() { return this.#implementation.lineStrokeWidth; }
+    set lineStrokeWidth(value) { this.#implementation.lineStrokeWidth = value; }
+    get circleStrokeWidth() { return this.#implementation.circleStrokeWidth; }
+    set circleStrokeWidth(value) { this.#implementation.circleStrokeWidth = value; }
+    get rectangleStrokeWidth() { return this.#implementation.rectangleStrokeWidth; }
+    set rectangleStrokeWidth(value) { this.#implementation.rectangleStrokeWidth = value; }
+
+    appendElement(element) {
+        this.#implementation.svg.appendChild(element);
+        return element;
+    } //appendElement
 
     line(x1, x2, y1, y2) {
         const element = this.#implementation.createNS("line");
         this.#implementation.attribute(element, {x1: x1, x2: x2, y1: y1, y2: y2});
         element.style.strokeWidth = this.#implementation.lineStrokeWidth;
         element.style.stroke = this.#implementation.lineStrokeColor;
-        this.#implementation.svg.appendChild(element);
-        return element;
+        return this.appendElement(element);
     } //line
+
+    circle(cx, cy, r) {
+        const element = this.#implementation.createNS("circle");
+        this.#implementation.attribute(element, {cx: cx, cy: cy, r: r});
+        this.#implementation.svg.appendChild(element);
+        element.style.stroke = this.#implementation.circleStrokeColor;
+        element.style.fill = this.#implementation.circleFillColor;
+        element.style.strokeWidth = this.#implementation.circleStrokeWidth;
+        return this.appendElement(element);
+    } //circle
+
+    rectangle(x, y, width, height, rx, ry) {
+        const element = this.#implementation.createNS("rect");
+        this.#implementation.attribute(element, {x: x, y: y, width: width, height: height});
+        if (rx) this.#implementation.attribute(element, {rx: rx});
+        if (ry) this.#implementation.attribute(element, {ry: ry});
+        element.style.stroke = this.#implementation.rectangleStrokeColor;
+        element.style.fill = this.#implementation.rectangleFillColor;
+        element.style.strokeWidth = this.#implementation.rectangleStrokeWidth;
+        return this.appendElement(element);
+    } //rectangle
+
+    text(x, y, value, size) {
+        const element = this.#implementation.createNS("text");
+        this.#implementation.attribute(element, {x: y, y: y});
+        element.textContent = value;
+        if (size) element.style.fontSize = size;
+        return this.appendElement(element);
+    } //text
 
 } //class SVG
