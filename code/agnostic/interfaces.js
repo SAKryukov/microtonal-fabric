2adbb6879b9de76c7df61abe1eb2829d5725c075
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2021-02-07 01:25:26 -0500

    Agnostic: added interfaces, the mechanism of interfaces for JavaScript

---
+++ code/agnostic/interfaces.js
@@ -0,0 +1,26 @@
+// Microtonal Fabric
+//
+// Copyright (c) Sergey A Kryukov, 2017-2021
+//
+// http://www.SAKryukov.org
+// http://www.codeproject.com/Members/SAKryukov
+// https://github.com/SAKryukov
+// https://github.com/SAKryukov/microtonal-fabric
+
+"use strict";
+
+class IInterface {
+    static isImplementedBy(testObject) {
+        const thisAsPrototype = Reflect.getPrototypeOf(new this());
+        const list = Reflect.ownKeys(thisAsPrototype);
+        for (let property of list) {
+            if (thisAsPrototype[property].constructor != Function) continue;
+            const test = testObject[property];
+            if (!test || test.constructor != Function) return false;
+        }
+        return true;
+    } //isImplementedBy
+    static throwNotImplemented(implementor) {
+        throw new Error(`${implementor.constructor.name} should implement ${this.name}`);
+    }
+} //class IInterface
