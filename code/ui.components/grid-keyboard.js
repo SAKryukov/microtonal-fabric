3a969813a58505cf4e11d4e363ef5927e8ef4e5a
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2021-02-10 03:05:11 -0500

    Code: ui.components GridKeyboard replaced with re-implemented version

--- code/ui.components/grid-keyboard.js
+++ code/ui.components/grid-keyboard.js
@@ -9,56 +9,26 @@
 
 "use strict";
 
-class GridKeyboard {
+class GridKeyboard extends AbstractKeyboard {
 
-    #implementation = { useKeyHightlight: true, rows: [], keyMap: new Map() };
+    testTest = { value: "value test" };
 
     constructor(element, keyWidth, keyHeight, rowCount, rowWidth, keyColors) {
-        if (!keyColors) keyColors = {
-            background: "transparent",
-            hightlight: "lightYellow",
-            disabled: "darkGray",
-            border: "black",
-            label: undefined
-        };
-        this.#implementation.label = handler => {
-            for (let [key, value] of this.#implementation.keyMap) {
-                const result = handler(value.x, value.y);
-                if (result == null) { // disabled key mechanism
-                    value.keyIndex = null;
-                    key.style.backgroundColor = keyColors.disabled;
-                } else
-                    key.textContent = result;
-            } //loop
-        }; //this.#implementation.label
-        this.#implementation.labelRow = (row, handler) => {
-            for (let index = 0; index < this.#implementation.rows[row].length; ++index) {
-                const element = this.#implementation.rows[row][index];
-                const value = this.#implementation.keyMap.get(element);
-                const result = handler(index);
-                if (result == null) { // disabled key mechanism
-                    value.keyIndex = null;
-                    element.style.backgroundColor = keyColors.disabled;
-                } else
-                    this.#implementation.rows[row][index].textContent = result;
-            } //loop
-        }; //this.#implementation.labelRow
-        this.#implementation.setTitles = handler => {
-            for (let [key, value] of this.#implementation.keyMap) {
-                const result = handler(value.x, value.y);
-                if (result)
-                    key.title = result;
-            } //loop
-        }; //this.#implementation.setTitles
-        this.#implementation.setRowTitles = (row, handler) => {
-            for (let index = 0; index < this.#implementation.rows[row].length; ++index) {
-                const element = this.#implementation.rows[row][index];
-                const value = this.#implementation.keyMap.get(element);
-                const result = handler(index);
-                if (result)
-                    element.title = result;
-            } //loop
-        }; //this.#implementation.setRowTitles
+        super(element, {keyWidth: keyWidth, keyHeight: keyHeight, rowCount: rowCount, rowWidth: rowWidth, keyColors: keyColors});
+    } //constructor
+
+    //IKeyboardGeometry:
+
+    createCustomKeyData(_, index) {
+        const metrics = this.derivedClassConstructorArguments[0];
+        const y = Math.floor(index / metrics.rowWidth);
+        const x = index % metrics.rowWidth;
+        return { x: x, y: y };
+    } //createCustomKeyData
+    createKeys(parentElement) {
+        this.derivedImplementation.rows = [];
+        const metrics = this.derivedClassConstructorArguments[0];
+        while (parentElement.firstChild) parentElement.removeChild(parentElement.lastChild);
         const parseSize = stringValue => {
             if (!stringValue) return { vale: undefined, unit: undefined };
             const size = (/([0-9]*\.?[0-9]*)([a-zA-Z]*)/).exec(stringValue);
@@ -67,86 +37,116 @@ class GridKeyboard {
             return { value: sizeValue, unit: sizeUnit };
         } //parseSize
         const keySize = (() => {
-            return { width: parseSize(keyWidth), height: parseSize(keyHeight) };
+            return {
+                width: parseSize(metrics.keyWidth),
+                height: parseSize(metrics.keyHeight)
+            };
         })();
-        element.style.display = "grid";
-        element.style.overflowX = "auto";
-        element.style.width = "100%";
-        const keyHandler = (target, on, isMove) => {
-            if (isMove && (event.buttons != 1)) return;
-            const keyData = this.#implementation.keyMap.get(target);
-            if (keyData.keyIndex == null) return; // indicated disabled
-            if (this.#implementation.useKeyHightlight)
-                target.style.backgroundColor = on ? keyColors.hightlight : keyColors.background;
-            if (this.#implementation.action)
-                this.#implementation.action(on, keyData.keyIndex);
-        }; //keyHandler
-        const borderStyle = `solid ${keyColors.border} thin`;
-        let keyIndex = 0;
-        for (let yIndex = 0; yIndex < rowCount; ++yIndex) {
-            this.#implementation.rows[yIndex] = [];
-            for (let xIndex = 0; xIndex < rowWidth; ++xIndex) {
+        this.derivedImplementation.defineGridTemplates = doFit => {
+            const widthUnit = doFit ? "fr" : keySize.width.unit;
+            const widthValue = doFit ? 1 : keySize.width.value;
+            parentElement.style.gridTemplateColumns = `repeat(${metrics.rowWidth}, ${widthValue}${widthUnit})`;
+        }; //this.derivedImplementation.defineGridTemplates
+        this.derivedImplementation.fitView = value => {
+            const fitWidth = parentElement.offsetWidth / metrics.rowWidth;
+            this.derivedImplementation.defineGridTemplates(value);
+            if (!(keySize.height.value && keySize.height.unit)) {
+                const keyHeight = value ? `${fitWidth}px` : `${keySize.width.value}${keySize.width.unit}`;
+                for (let key of parentElement.children)
+                    key.style.height = keyHeight;
+            }
+        } //this.derivedImplementation.fitView
+        this.derivedImplementation.fitView(false);
+        parentElement.style.backgroundColor = "white";
+        parentElement.style.display = "grid";
+        parentElement.style.overflowX = "auto";
+        parentElement.style.width = "100%";
+        const borderStyle = `solid ${metrics.keyColors.border} thin`;
+        for (let yIndex = 0; yIndex < metrics.rowCount; ++yIndex) {
+            this.derivedImplementation.rows[yIndex] = []; //SA???
+            for (let xIndex = 0; xIndex < metrics.rowWidth; ++xIndex) {
                 const key = document.createElement("div");
-                this.#implementation.rows[yIndex][xIndex] = key;
+                this.derivedImplementation.rows[yIndex][xIndex] = key;
                 key.style.borderRadius = "0px"; //SA???
                 key.style.paddingLeft = "0.3em";
                 key.style.paddingTop = "0.3em";
                 key.style.overflow = "hidden";
-                key.style.color = keyColors.label;
-                key.style.backgroundColor = keyColors.background;
-                key.style.height = keyHeight ? keyHeight : keyWidth;
+                key.style.color = metrics.keyColors.label;
+                key.style.backgroundColor = metrics.keyColors.background;
+                key.style.height = metrics.keyHeight ? metrics.keyHeight : metrics.keyWidth;
                 if (yIndex == 0)
                     key.style.borderTop = borderStyle;
                 if (xIndex == 0)
                     key.style.borderLeft = borderStyle;
                 key.style.borderBottom = borderStyle;
                 key.style.borderRight = borderStyle;
-                this.#implementation.keyMap.set(key, { x: xIndex, y: yIndex, keyIndex: keyIndex++ });
-                key.onmousedown = event => keyHandler(event.target, true);
-                key.onmouseup = event => keyHandler(event.target, false);
-                key.onmouseenter = event => keyHandler(event.target, true, true);
-                key.onmouseleave = event => keyHandler(event.target, false, true);
-                element.appendChild(key);
+                parentElement.appendChild(key);
             } //loop keys
         } //loop rows
-        (() => { //events
-            setMultiTouch(
-                element,
-                () => true,
-                (aKey, _, on) => keyHandler(aKey, on)
-            );
-        })(); //events
-        this.#implementation.defineGridTemplates = doFit => {
-            const widthUnit = doFit ? "fr" : keySize.width.unit;
-            const widthValue = doFit ? 1 : keySize.width.value;
-            element.style.gridTemplateColumns = `repeat(${rowWidth}, ${widthValue}${widthUnit})`;
-        }; //this.#implementation.defineGridTemplates
-        this.#implementation.defineGridTemplates(false);
-        let lastOffsetWidth = undefined;
-        this.#implementation.fitView = value => {
-            const fitWidth = element.offsetWidth / rowWidth;
-            this.#implementation.defineGridTemplates(value);
-            if (!(keySize.height.value && keySize.height.unit)) {
-                const keyHeight = value ? `${fitWidth}px` : `${keySize.width.value}${keySize.width.unit}`;
-                for (let key of this.#implementation.keyMap.keys())
-                    key.style.height = keyHeight;
-            }
-        };
-    } //constructor
+        this.derivedImplementation.label = handler => {
+            const metrics = this.derivedClassConstructorArguments[0];
+            for (let [key, value] of this.keyMap) {
+                const result = handler(value.customKeyData.x, value.customKeyData.y);
+                if (result == null) { // disabled key mechanism
+                    value.keyIndex = null;
+                    key.style.backgroundColor = metrics.keyColors.disabled;
+                } else
+                    key.textContent = result;
+            } //loop
+        }; //this.derivedImplementation.label
+        this.derivedImplementation.labelRow = (row, handler) => {
+            for (let index = 0; index < this.derivedImplementation.rows[row].length; ++index) {
+                const element = this.derivedImplementation.rows[row][index];
+                const value = this.keyMap.get(element);
+                const result = handler(index);
+                if (result == null) { // disabled key mechanism
+                    value.keyIndex = null;
+                    element.style.backgroundColor = keyColors.disabled;
+                } else
+                    this.derivedImplementation.rows[row][index].textContent = result;
+            } //loop
+        }; //this.derivedImplementation.labelRow
+        this.derivedImplementation.setTitles = handler => {
+            for (let [key, value] of this.keyMap) {
+                const result = handler(value.customKeyData.x, value.customKeyData.y);
+                if (result)
+                    key.title = result;
+            } //loop
+        }; //this.derivedImplementation.setTitles
+        this.derivedImplementation.setRowTitles = (row, handler) => {
+            for (let index = 0; index < this.derivedImplementation.rows[row].length; ++index) {
+                const element = this.derivedImplementation.rows[row][index];
+                const value = this.derivedImplementation.keyMap.get(element);
+                const result = handler(index);
+                if (result)
+                    element.title = result;
+            } //loop
+        }; //this.derivedImplementation.setRowTitles
+        return parentElement.children;
+    } //IKeyboardGeometry.createKeys
+    
+    highlightKey(keyElement, keyboardMode) {
+        const metrics = this.derivedClassConstructorArguments[0];
+        switch (keyboardMode) {
+            case keyHightlight.normal: keyElement.style.backgroundColor = metrics.keyColors.background; break;
+            case keyHightlight.down: keyElement.style.backgroundColor = metrics.keyColors.hightlight; break;
+            case keyHightlight.chord: keyElement.style.backgroundColor = metrics.keyColors.chord; break;
+            case keyHightlight.chordRoot: keyElement.style.backgroundColor = metrics.keyColors.chordRoot;
+        } //switch
+    } //IKeyboardGeometry.highlightKey
+    isTouchKey(parentElement, keyElement) {
+        return keyElement && keyElement.parentElement == parentElement;
+    } ////IKeyboardGeometry.isTouchKey
+    get defaultChord() {} // should return array of indices of keys in default chord
+    customKeyHandler(keyElement, keyData, on) {} // return false to stop embedded handling
+    
+    //IKeyboardGeometry
   
-    label(handler) { this.#implementation.label(handler); }
-    labelRow(row, handler) { this.#implementation.labelRow(row, handler); }
-    setTitles(handler) { this.#implementation.setTitles(handler); }
-    setRowTitles(row, handler) { this.#implementation.setRowTitles(row, handler); }
+    label(handler) { this.derivedImplementation.label(handler); }
+    labelRow(row, handler) { this.derivedImplementation.labelRow(row, handler); }
+    setTitles(handler) { this.derivedImplementation.setTitles(handler); }
+    setRowTitles(row, handler) { this.derivedImplementation.setRowTitles(row, handler); }
 
-    set fitView(booleanValue) { this.#implementation.fitView(booleanValue); }
-
-    set keyHandler(handler) { 
-        this.#implementation.action = handler; // handler(bool down, int keyIndex);
-    } //keyHandler
-    get action() { this.#implementation.action; }
-
-    get useHighlight() { return this.#implementation.useKeyHightlight; }
-    set useHighlight(value) { this.#implementation.useKeyHightlight = value; }
+    set fitView(booleanValue) { this.derivedImplementation.fitView(booleanValue); }
 
 } //class GridKeyboard
