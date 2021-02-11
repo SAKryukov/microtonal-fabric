33467bd5f7b54c652417632c08e27e4ef44f9874
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2021-02-11 10:17:20 -0500

    Code: ui.components: added GlobalTeyTracker, tested on Playground

---
+++ code/ui.components/global-key-tracker.js
@@ -0,0 +1,37 @@
+// Microtonal Fabric
+//
+// Copyright (c) Sergey A Kryukov, 2017-2021
+//
+// http://www.SAKryukov.org
+// http://www.codeproject.com/Members/SAKryukov
+// https://github.com/SAKryukov
+// https://github.com/SAKryukov/microtonal-fabric
+
+    const globalKeyTracker = {
+        keys: { control: 1, shift: 2, alt: 4 },
+        current: 0,
+        isControlDown: function() {
+            return (this.current & this.keys.control) > 0
+        },
+        isShiftDown: function() { return (this.current & this.keys.shift) > 0},
+        isAltDown: function() { return (this.current & this.keys.alt) > 0},
+        init: function() {
+            const thisObject = this;
+            window.addEventListener("keydown", function(event) {
+                if (event.ctrlKey)
+                    thisObject.current |= thisObject.keys.control;
+                if (event.shiftKey)
+                    thisObject.current |= thisObject.keys.shift;
+                if (event.altKey)
+                    thisObject.current |= thisObject.keys.alt;
+            });
+            window.addEventListener("keyup", function(event) {
+                if (!event.ctrlKey)
+                    thisObject.current &= ~thisObject.keys.control;
+                if (!event.shiftKey)
+                    thisObject.current &= ~thisObject.keys.shift;
+                if (!event.altKey)
+                    thisObject.current &= ~thisObject.keys.alt;
+            })
+        },
+    }; //globalKeyTracker
