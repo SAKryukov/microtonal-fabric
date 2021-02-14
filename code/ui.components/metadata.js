6168f0758c0db50eed2ad4e47811878d2fad7274
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2021-02-13 23:13:03 -0500

    Code: ui.components: new metadata option: product only, it means that author/copyright is shown separately

--- code/ui.components/metadata.js
+++ code/ui.components/metadata.js
@@ -8,7 +8,7 @@
 // https://github.com/SAKryukov/microtonal-fabric
 
 const metadata = {
-    initialize: function(parentElement) {
+    initialize: function(parentElement, productOnly) {
         const title = document.title;
         const version = sharedDefinitionSet.version;
         const years = sharedDefinitionSet.years;
@@ -23,6 +23,8 @@ const metadata = {
         if (url.length < 2) throw new Error("HTML urls are not found; the application needs product and author URLs");
         if (!author) throw new Error("HTML author not found");
         parentElement.title = document.title;
-        parentElement.innerHTML = `<a href="${url[0]}">${title}</a> v.&thinsp;${version}<span style="padding: 1em">&bigstar;</span>Copyright &copy; ${years} <a href="${url[1]}">${author}</a>`
+        parentElement.innerHTML = productOnly
+            ? `<a href="${url[0]}">${title}</a> v.&thinsp;${version}`
+            : `<a href="${url[0]}">${title}</a> v.&thinsp;${version}<span style="padding: 1em">&bigstar;</span>Copyright &copy; ${years} <a href="${url[1]}">${author}</a>`;
     },
 }; //metadata
