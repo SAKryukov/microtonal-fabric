e54cc01d49f02ea547e7ef038b34d171b05c314b
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2021-02-13 21:58:54 -0500

    Code: ui.components: metadata element

--- code/ui.components/global-key-tracker.js
+++ code/ui.components/global-key-tracker.js
@@ -7,29 +7,29 @@
 // https://github.com/SAKryukov
 // https://github.com/SAKryukov/microtonal-fabric
 
-    const globalKeyTracker = {
-        keys: { control: 1, shift: 2, alt: 4 },
-        current: 0,
-        isControlDown: function() { return (this.current & this.keys.control) > 0 },
-        isShiftDown: function() { return (this.current & this.keys.shift) > 0 },
-        isAltDown: function() { return (this.current & this.keys.alt) > 0 },
-        init: function() {
-            const thisObject = this;
-            window.addEventListener("keydown", function(event) {
-                if (event.ctrlKey)
-                    thisObject.current |= thisObject.keys.control;
-                if (event.shiftKey)
-                    thisObject.current |= thisObject.keys.shift;
-                if (event.altKey)
-                    thisObject.current |= thisObject.keys.alt;
-            });
-            window.addEventListener("keyup", function(event) {
-                if (!event.ctrlKey)
-                    thisObject.current &= ~thisObject.keys.control;
-                if (!event.shiftKey)
-                    thisObject.current &= ~thisObject.keys.shift;
-                if (!event.altKey)
-                    thisObject.current &= ~thisObject.keys.alt;
-            })
-        },
-    }; //globalKeyTracker
+const globalKeyTracker = {
+    keys: { control: 1, shift: 2, alt: 4 },
+    current: 0,
+    isControlDown: function() { return (this.current & this.keys.control) > 0 },
+    isShiftDown: function() { return (this.current & this.keys.shift) > 0 },
+    isAltDown: function() { return (this.current & this.keys.alt) > 0 },
+    init: function() {
+        const thisObject = this;
+        window.addEventListener("keydown", function(event) {
+            if (event.ctrlKey)
+                thisObject.current |= thisObject.keys.control;
+            if (event.shiftKey)
+                thisObject.current |= thisObject.keys.shift;
+            if (event.altKey)
+                thisObject.current |= thisObject.keys.alt;
+        });
+        window.addEventListener("keyup", function(event) {
+            if (!event.ctrlKey)
+                thisObject.current &= ~thisObject.keys.control;
+            if (!event.shiftKey)
+                thisObject.current &= ~thisObject.keys.shift;
+            if (!event.altKey)
+                thisObject.current &= ~thisObject.keys.alt;
+        })
+    },
+}; //globalKeyTracker
---
+++ code/ui.components/metadata.js
@@ -0,0 +1,26 @@
+// Microtonal Fabric
+//
+// Copyright (c) Sergey A Kryukov, 2017-2021
+//
+// http://www.SAKryukov.org
+// http://www.codeproject.com/Members/SAKryukov
+// https://github.com/SAKryukov
+// https://github.com/SAKryukov/microtonal-fabric
+
+const metadata = {
+    initialize: function(parentElement) {
+        const title = document.title;
+        const version = sharedDefinitionSet.version;
+        const years = sharedDefinitionSet.years;
+        const meta = document.querySelectorAll("meta");
+        let author = "", url = [];
+        for (let element of meta) {
+            if (element.name == "author")
+                author = element.content;
+            if (element.name == "url")
+                url.push(element.content);
+        } //loop
+        if (url.length < 2) throw new Error("HTML urls are not found; the application needs product and author URLs");
+        if (!author) throw new Error("HTML author not found");
+    },
+}; //metadata
