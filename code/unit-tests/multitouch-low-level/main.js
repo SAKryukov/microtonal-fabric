d6240b4b4ddb80351c2e15092311d451acadf3e8
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-06-04 12:23:40 -0400

    Code: started unit-tests/multitouch-low-level

---
+++ code/unit-tests/multitouch-low-level/main.js
@@ -0,0 +1,15 @@
+// Microtonal Fabric
+//
+// Copyright (c) Sergey A Kryukov, 2017-2023
+//
+// http://www.SAKryukov.org
+// http://www.codeproject.com/Members/SAKryukov
+// https://github.com/SAKryukov
+// https://github.com/SAKryukov/microtonal-fabric
+
+"use strict";
+
+window.onload = () => {
+
+    
+} //window.onload
