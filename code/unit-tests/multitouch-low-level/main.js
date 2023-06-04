f4899bf1f8525b06bfdc62310b88f11cb3f829be
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-06-04 17:11:33 -0400

    Code: low-level touch test complete

--- code/unit-tests/multitouch-low-level/main.js
+++ code/unit-tests/multitouch-low-level/main.js
@@ -11,5 +11,44 @@
 
 window.onload = () => {
 
+    const elements = {
+        keyboard: document.querySelector("body > div"),
+        keys: [ document.querySelector("body > div > b:first-of-type"), document.querySelector("body > div > b:last-of-type") ],
+        buttonClear: document.querySelector("button"),
+        output: document.querySelector("select"),
+    };
+
+    const writeLine = text => {
+        const option = document.createElement("option");
+        option.textContent = text;
+        elements.output.appendChild(option);
+    };
+    const assignEvent = (element, name, handler) => {
+        element.addEventListener(name, handler, { passive: false, capture: true });
+    };
+    const assignTouchStart = (element, handler) => {
+        assignEvent(element, "touchstart", handler);
+    };
+    const assignTouchMove = (element, handler) => {
+        assignEvent(element, "touchmove", handler);
+    };
+    const assignTouchEnd = (element, handler) => {
+        assignEvent(element, "touchend", handler);
+    };
+    const assignTouchCancel = (element, handler) => {
+        assignEvent(element, "touchcancel", handler);
+    };
+
+    for (let element of elements.keys) {
+        assignTouchStart(element, ()=> writeLine("start"));
+        assignTouchEnd(element, ()=> writeLine("end"));
+        assignTouchCancel(element, ()=> writeLine("cancel"));
+        assignTouchMove(element, ()=> writeLine("move"));    
+    } //loop
+
+    elements.buttonClear.onclick = () => {
+        while(elements.output.children.length > 0)
+            elements.output.removeChild(elements.output.lastChild);
+    };
     
 } //window.onload
