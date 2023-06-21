3bd04b771725c808414b257d4b27668a69ff22de
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-06-20 22:10:28 -0400

    Code: tests: plucking prototype: volume found

--- code/unit-tests/plucked-string/keyboard.js
+++ code/unit-tests/plucked-string/keyboard.js
@@ -1,8 +1,2 @@
-class StringKeyboard extends AbstractKeyboard {
-    createKeys(parentElement) { return []; }
-    createCustomKeyData(keyElement, index) {}
-    highlightKey(keyElement, keyboardMode) {}
-    isTouchKey(parentElement, keyElement) { return false; } // for touch interface
-    get defaultChord() {} // should return array of indices of keys in default chord
-    customKeyHandler(keyElement, keyData, on) {} // return false to stop embedded handling
-} //class StringKeyboard
+"use strict";
+
--- code/unit-tests/plucked-string/main.js
+++ code/unit-tests/plucked-string/main.js
@@ -28,16 +28,36 @@ window.onload = () => {
     svg.polygonStrokeColor = "blue";
     //svg.polygonFillColor = 
     svg.polygonStrokeWidth = DefinitionSet.thickness;
-    svg.polygonStrokeLineJoin = StrokeJoin.bevel;
 
     svg.lineStrokeWidth = DefinitionSet.thickness;
     svg.line(-1, 1, 0, 0);
     //svg.line(-1, 1, -DefinitionSet.widthLeft, -DefinitionSet.widthRight);
     //svg.line(-1, 1, +DefinitionSet.widthLeft, +DefinitionSet.widthRight);
-    const key = svg.polygon([[-1, -DefinitionSet.widthLeft], [1, -DefinitionSet.widthRight], [1, +DefinitionSet.widthRight], [-1, +DefinitionSet.widthLeft]])
+    const key = svg.polygon([[-1, -DefinitionSet.widthLeft], [1, -DefinitionSet.widthRight], [1, +DefinitionSet.widthRight], [-1, +DefinitionSet.widthLeft]]);
 
     document.body.appendChild(svg.element);
 
-    const keyboard = new StringKeyboard();
-    
+    let lastElement = null;
+    let lastCoordinate = null;
+
+    const play = (element, volumeDelta) => {
+        const keySize = DefinitionSet.widthRight * 2 * (svg.element.getBoundingClientRect().height / DefinitionSet.halfSize / 2);
+        console.log(element, "Relative: " + Math.abs(volumeDelta)/keySize);
+    } //play
+
+    setMultiTouch(
+        svg.element,
+        element => element.constructor == SVGPolygonElement,
+        (element, touchObject, on) => {
+            if (on) {
+                lastElement = element;
+                lastCoordinate = touchObject.clientY;
+            } else {
+                if (lastElement == element)
+                    play(element, touchObject.clientY - lastCoordinate);
+            } //if
+        },
+        (element, touchObject) => { //same element
+        });
+
 } //window.onload
